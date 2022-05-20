233c9aec9f9095def1a34668694decb6619630c7
alexliou <alexliou0@gmail.com>
2022-05-20 15:24:36 -0400

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Alex Liou on 5/20/22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
